055ebb6290c2547d40e9f57fb31744f7e443e6ff
DoubleV37 <valentin.v3719@gmail.com>
2023-12-13 17:01:01 +0100

    Add a new index page to learn how add html with django

--- Backend/Django/docker-entrypoint.sh
+++ Backend/Django/docker-entrypoint.sh
@@ -6,5 +6,8 @@ cd /app/backend
 
 mv /app/settings.py /app/backend/backend/settings.py
 mv /app/urls.py /app/backend/backend/urls.py
+mv /app/views.py /app/backend/backend/views.py
+mv /app/manage.py /app/backend/manage.py
+mv /app/templates /app/backend/templates
 
 exec python3 manage.py runsslserver 0.0.0.0:8000
--- Backend/Django/settings.py
+++ Backend/Django/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
