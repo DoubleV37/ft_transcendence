066210aa744c7ada6342ab224c522597e2c95da4
DoubleV37 <valentin.v3719@gmail.com>
2023-12-19 12:32:54 +0100

    Improve server files structure and use of django, error 404 is now a custom page with an image of 404 Peugeot and use of app django instead of use django project for everything

--- Backend/Django/docker-entrypoint.sh
+++ Backend/Django/docker-entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-django-admin startproject backend
-
 cd /app/backend
-
-mv /app/settings.py /app/backend/backend/settings.py
-mv /app/urls.py /app/backend/backend/urls.py
-mv /app/views.py /app/backend/backend/views.py
-mv /app/manage.py /app/backend/manage.py
-mv /app/templates /app/backend/templates
 
 exec python3 manage.py runsslserver 0.0.0.0:8000
