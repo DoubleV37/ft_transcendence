bcfb1692381283190ac6c6bc0f3654d18ed3aa9d
DoubleV37 <valentin.v3719@gmail.com>
2023-12-20 15:23:49 +0100

    config database in django

--- Backend/Django/project_backend/backend/settings.py
+++ Backend/Django/project_backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+		'NAME': config('POSTGRES_DB'),
+		'USER': config('POSTGRES_USER'),
+		'PASSWORD': config('POSTGRES_PASSWORD'),
+		'HOST': config('POSTGRES_HOST'),
+		'PORT': config('POSTGRES_PORT')
     }
 }
 
