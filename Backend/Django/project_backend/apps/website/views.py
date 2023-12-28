8ebcf5fc4713c04842935b75338d33c24a95e5af
DoubleV37 <valentin.v3719@gmail.com>
2023-12-28 21:14:37 +0100

    add files to implement first single page application in the website

--- Backend/Django/project_backend/apps/website/views.py
+++ Backend/Django/project_backend/apps/website/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class UNView(TemplateView):
+	template_name = "1.html"
+
+class DEUXView(TemplateView):
+	template_name = "2.html"
+
+class TROISView(TemplateView):
+	template_name = "3.html"
+
+pages = [UNView, DEUXView, TROISView]
+
+def section(request, num = 0):
+	if num == 0:
+		return IndexView.as_view()(request)
+	elif 1 <= num <= 3:
+		return pages[num-1].as_view()(request)
+	else:
+		raise Http404("No such section")
