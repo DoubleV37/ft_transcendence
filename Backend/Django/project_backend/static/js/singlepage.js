c7af54cc726bbe868ba6212a1c5ef44bcb234179
Valentin Viovi <vviovi@2D2.42angouleme.fr>
2024-01-15 11:31:49 +0100

    The SPA work with history and reload

--- Backend/Django/project_backend/static/js/singlepage.js
+++ Backend/Django/project_backend/static/js/singlepage.js
@@ -6,45 +6,35 @@ function showSection(section) {
 		tempDiv.innerHTML = text;
 
 		// Récupérer le contenu de la balise avec l'ID 'content' de la page fetchée
-		var fetchedContent = tempDiv.querySelector('#content'); //ya pb la
+		var fetchedContent = tempDiv.querySelector('#content');
 
-		// Insérer le contenu dans la balise avec l'ID 'content' de la page actuelle
-		var currentContentElement = document.querySelector('#content');
-		currentContentElement.innerHTML = fetchedContent.innerHTML;
-		// document.querySelector('#content').innerHTML = text;
+		// Remplacer le contenu de la balise avec l'ID 'content' de la page actuelle par le contenu de la balise avec l'ID 'content' de la page fetchée
+		document.querySelector('#content').innerHTML = fetchedContent.innerHTML;
 	});
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-	if (page != 'index') {
-		fetch('header')
-		.then(response => response.text())
-		.then(text => {
-			document.querySelector('#header').innerHTML = text;
-		});
-		fetch('footer')
-		.then(response => response.text())
-		.then(text => {
-			document.querySelector('#footer').innerHTML = text;
-		});
-		page = 'index'
-	}
-
-	document.querySelectorAll('button').forEach(button => {
-		button.onclick = function() {
-			showSection(this.dataset.section)
-			history.pushState({section: this.dataset.section}, '', this.dataset.section)
-			window.onpopstate = function(event) {
-				showSection(event.state.section)
-			}
-		}
-	})
+	fetch('header')
+	.then(response => response.text())
+	.then(text => {
+		document.querySelector('#header').innerHTML = text;
+	});
+	fetch('footer')
+	.then(response => response.text())
+	.then(text => {
+		document.querySelector('#footer').innerHTML = text;
+	});
 });
 
 function clickButton(button) {
 	showSection(button.dataset.section)
 	history.pushState({section: button.dataset.section}, '', button.dataset.section)
-	window.onpopstate = function(event) {
-		showSection(event.state.section)
+}
+
+window.onpopstate = function(event) {
+	if (event.state == null) {
+		showSection('')
+		return
 	}
+	showSection(event.state.section)
 }
