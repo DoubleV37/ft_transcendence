0e4f36e41ff3807376cf3a8a043e237a1715c25c
DoubleV37 <valentin.v3719@gmail.com>
2024-01-26 19:08:20 +0100

    implementation of the v1 database with django

--- Django_data/apps/website/models.py
+++ Django_data/apps/website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class User():
+	id = models.AutoField(primary_key=True)
+	username = models.CharField(max_length=50)
+	email = models.CharField(max_length=50, unique=True)
+	password = models.CharField(max_length=50)
+	avatar = models.BitmapField()
+
+class Messages():
+	id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	body = models.CharField(max_length=500)
+	send_at = models.DateTimeField(auto_now_add=True)
+
+class Stats():
+	id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	nb_games = models.IntegerField()
+	nb_wins = models.IntegerField()
+
+class Links():
+	user_id1 = models.ForeignKey(User, on_delete=models.CASCADE)
+	user_id2 = models.ForeignKey(User, on_delete=models.CASCADE)
+	link = models.CharField(max_length=50)
+
