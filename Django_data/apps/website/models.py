c39f4f511b2dcbaff288a4de7f7deb027b096d59
DoubleV37 <valentin.v3719@gmail.com>
2024-01-28 00:32:27 +0100

    fix bug in the database file

--- Django_data/apps/website/models.py
+++ Django_data/apps/website/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-class User():
+class User(models.Model):
 	id = models.AutoField(primary_key=True)
 	username = models.CharField(max_length=50)
 	email = models.CharField(max_length=50, unique=True)
 	password = models.CharField(max_length=50)
-	avatar = models.BitmapField()
+	avatar = models.CharField(max_length=50)
 
-class Messages():
+class Messages(models.Model):
 	id = models.AutoField(primary_key=True)
 	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 	body = models.CharField(max_length=500)
 	send_at = models.DateTimeField(auto_now_add=True)
 
-class Stats():
+class Stats(models.Model):
 	id = models.AutoField(primary_key=True)
 	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 	nb_games = models.IntegerField()
 	nb_wins = models.IntegerField()
 
-class Links():
+class Links(models.Model):
 	user_id1 = models.ForeignKey(User, on_delete=models.CASCADE)
 	user_id2 = models.ForeignKey(User, on_delete=models.CASCADE)
 	link = models.CharField(max_length=50)
