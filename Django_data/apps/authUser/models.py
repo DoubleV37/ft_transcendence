500667a983ea63c75e44190160e8659026dbf672
DoubleV37 <valentin.v3719@gmail.com>
2024-02-06 08:52:11 +0100

    add idUser

--- Django_data/apps/authUser/models.py
+++ Django_data/apps/authUser/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
+    idUser = models.AutoField(primary_key=True)
     status = models.BooleanField(default=True)
-    username = models.CharField(primary_key=True, max_length=50, unique=True, null=False)
-    email = models.CharField(max_length=50,unique=True, null=False)
-    password = models.CharField(null=False)
-    avatar = models.ImageField()
+    username = models.CharField(max_length=50, unique=True, null=False)
+    email = models.EmailField(max_length=50, unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+    avatar = models.ImageField(upload_to='avatars/')
 
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
