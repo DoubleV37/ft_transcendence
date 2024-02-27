361baecb299d1f112739014074e4a4c2c356d457
Guillaume Azzopardi <gazzopar@1G3.42angouleme.fr>
2024-02-27 18:01:44 +0100

    Mourir : une bonne solution ou pas ?

--- Django_data/apps/Profile/urls.py
+++ Django_data/apps/Profile/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('profilext/', views.profilext, name='profilext'),
+    path('profileformat/', views.settings, name='profileformat'),
     path('settings/', views.settings, name='settings'),
 ]
