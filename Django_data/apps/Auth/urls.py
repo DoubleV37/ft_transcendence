fc87492e53916360d7c893b29bec764659953996
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2024-02-29 14:34:47 +0100

    CLEANING(AUTH/URLS.PY)
    -> Suppression of unused path

--- Django_data/apps/Auth/urls.py
+++ Django_data/apps/Auth/urls.py
@@ -5,12 +5,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
-    path('info/', views.info, name='info'),
-    path('edit_name/', views.edit_name, name='edit_name'),
-    path('edit_psswd/', views.edit_psswd, name='edit_psswd'),
-    path('edit_mail/', views.edit_mail, name='edit_mail'),
-    path('edit_avatar/', views.edit_avatar, name='edit_avatar'),
-    path('edit_allInfo/', views.allinfo, name='allinfo'),
 
     path('settings/', views.my_settings, name='my_settings'),
 ]
