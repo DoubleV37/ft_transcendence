640e7efdf5f5d92f4b176b65098a4f89563a8001
Guillaume Azzopardi <gazzopar@1H1.42angouleme.fr>
2024-03-05 16:43:00 +0100

    Double button signin / signup on home

--- Django_data/static/js/Home/Home_Events.js
+++ Django_data/static/js/Home/Home_Events.js
@@ -1,15 +1,25 @@
 function  home_SetEvents() {
-  let element = document.getElementById('PlayButton');
 
-  element.addEventListener('click', PlayCallBack);
+  let element = document.getElementById('SigninButton');
+  element.addEventListener('click', SigninCallBack);
+
+  element = document.getElementById('SignupButton');
+  element.addEventListener('click', SignupCallBack);
 }
 
 function  home_DelEvents() {
-  let element = document.getElementById('PlayButton');
 
-  element.removeEventListener('click', PlayCallBack);
+  let element = document.getElementById('SigninButton');
+  element.removeEventListener('click', SigninCallBack);
+
+  element = document.getElementById('SignupButton');
+  element.removeEventListener('click', SignupCallBack);
 }
 
-function  PlayCallBack() {
-    console.log("Do nothing for now");
+function  SigninCallBack() {
+  loadPage(`${ROUTE.SIGNIN}`);
+}
+
+function  SignupCallBack() {
+  loadPage(`${ROUTE.SIGNUP}`);
 }
