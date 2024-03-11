08ddfaebac5dfa9c4ebfbf6baaf69140072d312e
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2024-03-11 15:13:17 +0100

    remove on used code

--- Django_data/apps/Twofa/forms.py
+++ Django_data/apps/Twofa/forms.py
@@ -17,34 +17,3 @@ class TwoFAForm(forms.ModelForm):
     class Meta:
         model = UserTwoFA
         fields = ('otp',)
-
-        # return True
-
-    # def get_form_class(self):
-    #     return self.Form
-
-    # def get_form(self, *args, **kwargs):
-    #     form = super().get_form(*args, **kwargs)
-    #
-    #     form.user = self.request.user
-    #
-    #     return form
-
-    # def form_valid(self, form):
-    #     return super().form_valid(form)
-
-    # def clean_otp(self):
-    #     self.two_factor_auth_data = UserTwoFactorAuthData.objects.filter(
-    #         user=self.user
-    #     ).first()
-    #
-    #     if self.two_factor_auth_data is None:
-    #         raise ValidationError('2FA not set up.')
-    #
-    #     otp = self.cleaned_data.get('otp')
-    #
-    #     if not self.two_factor_auth_data.validate_otp(otp):
-    #         raise ValidationError('Invalid 2FA code.')
-    #
-    #     return otp
-    #
