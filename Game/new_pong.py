f77972effddc5289dd4fdaf99dc341e3836b7a8e
Charles Masserann <charloxd@gmail.com>
2024-03-14 09:21:00 +0100

    first dictionary

--- Game/new_pong.py
+++ Game/new_pong.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 import math
+import json
+import time
+
 
 # Global settings
 window_width = 1200
@@ -16,7 +19,7 @@ paddle_radius = 8
 IA_lvl = 20
 
 pygame.init()
-screen = pygame.display.set_mode((window_width, window_height))
+screen = pygame.display.set_mode((1200, 900))
 clock = pygame.time.Clock()
 pygame.display.set_caption("PONG")
 
@@ -34,18 +37,27 @@ IA_target = window_height / 2
 IA_counter = 0
 ball_speed = pygame.Vector2( [3, random.uniform(-1, 1)] )
 ball_pos = pygame.Vector2( paddle_padding, window_height / 2)
-ball_old_pos = []
 IA_random = random.randint( -IA_lvl, IA_lvl )
 
 
 for_json = {}
 for_json["running"] = True
-for_json["point"] = point
+for_json["point"] = [0, 0]
 for_json["lplayer_pos"] = lplayer_pos
+for_json["lplayer_size"] = paddle_height
 for_json["rplayer_pos"] = rplayer_pos
+for_json["rplayer_size"] = paddle_height
 for_json["player_speed"] = paddle_speed
 for_json["ball_pos"] = ball_pos
 for_json["ball_speed"] = ball_speed
+
+def widow(data, screen, clock):
+    screen.fill("black")
+    pygame.draw.rect(screen, "white", (598, 0, 4, 900))
+    pygame.draw.rect(screen, "white", (40, data["lplayer_pos"] - (data["lplayer_size"] / 2), 10, data["lplayer_size"]))
+    pygame.draw.rect(screen, "white", (1150, data["rplayer_pos"] - (data["rplayer_size"] / 2), 10, data["rplayer_size"]))
+    pygame.draw.circle(screen, "white", data["ball_pos"], 10)
+    pygame.display.flip()
 
 
 def path_estimation( player, pos , speed ):
@@ -98,53 +110,29 @@ while running:
         running = False
         print_stat()
     
-    if keys[pygame.K_UP] and lplayer_pos > 0:
-        lplayer_pos -= paddle_speed
+    if keys[pygame.K_UP] and rplayer_pos > 0:
+        rplayer_pos -= paddle_speed
 
-    elif keys[pygame.K_DOWN] and lplayer_pos < window_height:
-        lplayer_pos += paddle_speed
-
-    screen.fill("black")
-    
-    # draw middle line
-    pygame.draw.rect(screen, "white", (window_width / 2 - 2, 0, 4, window_height))
-
-    # draw IA
-    IA = pygame.draw.rect(screen, "white", ( paddle_padding - paddle_width, IA_pos - (paddle_height / 2), paddle_width, paddle_height))
-
-    # draw ball
-    ball = pygame.draw.circle(screen, "white", ball_pos, 2 * ball_radius)
-
-    # draw player
-    player = pygame.draw.rect(screen, "white", ( window_width - paddle_padding, lplayer_pos - (paddle_height / 2), paddle_width, paddle_height))
-
-    # draw ball trail
-    ball_old_pos.append(ball_pos.copy())
-    if len(ball_old_pos) > 100:
-        ball_old_pos.pop(0)
-    raptici = 0
-    for i in range(1, len(ball_old_pos)):
-        raptici += 12/100 
-        pygame.draw.circle(screen, "white", ball_old_pos[i], raptici)
-
+    elif keys[pygame.K_DOWN] and rplayer_pos < window_height:
+        rplayer_pos += paddle_speed
 
     # ball move
     ball_pos += ball_speed
 
-    if (paddle_padding + ball_radius) > ball_pos.x > ((paddle_padding + ball_radius) + ( 2 * ball_speed.x)) and ball_speed.x < 0 and IA_pos - (paddle_height / 2 + ball_radius) < ball_pos.y < IA_pos + (paddle_height / 2 + ball_radius):
+    if (paddle_padding + ball_radius) > ball_pos.x > ((paddle_padding + ball_radius) + ( 2 * ball_speed.x)) and ball_speed.x < 0 and lplayer_pos - (paddle_height / 2 + ball_radius) < ball_pos.y < lplayer_pos + (paddle_height / 2 + ball_radius):
         ball_speed.x *= -1
         ball_speed.x += ball_acceleration
-        ball_speed.y = (ball_pos.y - IA_pos) / paddle_radius
+        ball_speed.y = (ball_pos.y - lplayer_pos) / paddle_radius
         IA_random = random.randint( -IA_lvl, IA_lvl )
         exchange += 1
         ball_bonce += 1
         if math.sqrt( ball_speed.x ** 2 + ball_speed.y ** 2 ) > ball_max_speed[0]:
             ball_max_speed[0] = math.sqrt( ball_speed.x ** 2 + ball_speed.y ** 2 )
 
-    elif (window_width - paddle_padding - paddle_width + ball_radius) < ball_pos.x < ((window_width - paddle_padding - paddle_width + ball_radius) + ( 2 * ball_speed.x)) and ball_speed.x > 0 and lplayer_pos - (paddle_height / 2 + ball_radius) < ball_pos.y < lplayer_pos + (paddle_height / 2 + ball_radius):
+    elif (window_width - paddle_padding - paddle_width + ball_radius) < ball_pos.x < ((window_width - paddle_padding - paddle_width + ball_radius) + ( 2 * ball_speed.x)) and ball_speed.x > 0 and rplayer_pos - (paddle_height / 2 + ball_radius) < ball_pos.y < rplayer_pos + (paddle_height / 2 + ball_radius):
         ball_speed.x *= -1
         ball_speed.x -= ball_acceleration
-        ball_speed.y = (ball_pos.y - lplayer_pos) / paddle_radius
+        ball_speed.y = (ball_pos.y - rplayer_pos) / paddle_radius
         IA_random = random.randint( -IA_lvl, IA_lvl )
         exchange += 1
         ball_bonce += 1
@@ -158,7 +146,7 @@ while running:
     if ball_pos.x > window_width:
         ball_pos = pygame.Vector2( window_width - paddle_padding, window_height / 2 )
         ball_speed = pygame.Vector2(-3, random.uniform(-1, 1))
-        IA_pos = window_height / 2
+        lplayer_pos = window_height / 2
         lplayer_pos = window_height / 2
         point[0] += 1
         print(f"{point[0]} IA | Player {point[1]}")
@@ -171,7 +159,7 @@ while running:
     if ball_pos.x < 0:
         ball_pos = pygame.Vector2( paddle_padding, window_height / 2 )
         ball_speed = pygame.Vector2(3, random.uniform(-1, 1))
-        IA_pos = window_height / 2
+        lplayer_pos = window_height / 2
         lplayer_pos = window_height / 2
         point[1] += 1
         print(f"{point[0]} IA | Player {point[1]}")
@@ -191,9 +179,13 @@ while running:
     if IA_counter > 240:
         IA_counter = 0
     
-    IA_pos = paddle_move( IA_pos, IA_target, paddle_speed )
+    lplayer_pos = paddle_move( lplayer_pos, IA_target, paddle_speed )
 
-    pygame.display.flip()
+    # pygame.display.flip()
+    for_json["lplayer_pos"] = lplayer_pos
+    for_json["rplayer_pos"] = rplayer_pos
+    for_json["ball_pos"] = ball_pos
+    widow(for_json, screen, clock)
 
     clock.tick(240) / 1000
 
