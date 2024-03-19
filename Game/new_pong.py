3ae11323da0b6138d9e9d409efa62db25ffac257
cmassera <cmassera@student.42angouleme.fr>
2024-03-19 11:33:52 +0100

    debug is comming

--- Game/new_pong.py
+++ Game/new_pong.py
@@ -1,8 +1,6 @@
 import pygame
 import random
 import math
-import json
-import time
 
 def widow(Pong, screen, clock):
     screen.fill("black")
@@ -11,36 +9,30 @@ def widow(Pong, screen, clock):
     pygame.draw.rect(screen, "white", (1150, Pong.player_pos[1] - (Pong.player_size[1] / 2), 10, Pong.player_size[1]))
     pygame.draw.circle(screen, "white", (Pong.ball_pos[0], Pong.ball_pos[1]), 12)
     pygame.display.flip()
-    clock.tick(240) / 1000
+    clock.tick(240)
 
 
-def path_estimation( player, pos , speed, lvl ):
+def ai_brain( Pong, player, lvl ):
 
     lvl = random.randint( -lvl, lvl )
-    if player == 1:
-        if speed.x > 0:
+    if player == 0:
+        if Pong.ball_speed[0] > 0:
             return 450
-        while pos.x > 60:
-            pos += speed
-            if 5 > pos.y or pos.y > 895:
-                speed.y *= -1
-        return pos.y + lvl
+        while Pong.ball_pos[1] > 60:
+            Pong.ball_pos[0] += Pong.ball_speed[0]
+            Pong.ball_pos[1] += Pong.ball_speed[1]
+            if 5 > Pong.ball_pos[1] or Pong.ball_pos[1] > 895:
+                Pong.ball_speed[1] *= -1
+        return Pong.ball_pos[1] + lvl
 
-    if speed.x < 0:
+    if Pong.ball_speed[0] < 0:
         return 450
-    while pos.x < 1150:
-        pos += speed
-        if 5 > pos.y or pos.y > 895:
-            speed.y *= -1
-    return pos.y + lvl
-
-
-def paddle_move( pos, target, speed ):
-    if 0 < pos > target + speed:
-        pos -= speed
-    elif 900 > pos < target - speed:
-        pos += speed
-    return pos
+    while Pong.ball_pos[1] < 1150:
+        Pong.ball_pos[0] += Pong.ball_speed[0]
+        Pong.ball_pos[1] += Pong.ball_speed[1]
+        if 5 > Pong.ball_pos[1] or Pong.ball_pos[1] > 895:
+            Pong.ball_speed[1] *= -1
+    return Pong.ball_pos[1] + lvl
 
 
 class Pong():
@@ -75,7 +67,7 @@ class Pong():
             self.ball_max_speed[player] = speed
 
 
-    def point(self, player):
+    def update_score(self, player):
         self.point[player] += 1
         self.player_pos[0] = 450
         self.player_pos[1] = 450
@@ -123,12 +115,12 @@ class Pong():
             if self.point[0] == self.point_limit or self.point[1] == self.point_limit or keys[pygame.K_ESCAPE]:
                 self.running = False
                 self.print_stats()
-            if keys[pygame.K_UP] and self.rplayer_pos > 0:
+            if keys[pygame.K_UP] and self.player_pos[1] > 0:
                 self.player_pos[1] -= self.player_speed
-            elif keys[pygame.K_DOWN] and self.rplayer_pos < 900:
+            elif keys[pygame.K_DOWN] and self.player_pos[1] < 900:
                 self.player_pos[1] += self.player_speed
             # ia move
-            self.player_pos[0] = paddle_move( self.player_pos[0], path_estimation( 1, self.ball_pos.copy(), self.ball_speed.copy(), 20 ), self.player_speed )
+            self.player_pos[0] = ai_brain(self, 0, 20)
             # ball move
             self.ball_pos[0] += self.ball_speed[0]
             self.ball_pos[1] += self.ball_speed[1]
@@ -143,9 +135,9 @@ class Pong():
                 self.ball_bonce += 1
             # point
             if self.ball_pos[0] > 1200:
-                self.point(0)
+                self.update_score(0)
             if self.ball_pos[0] < 0:
-                self.point(1)
+                self.update_score(1)
             # pygame display #
             widow(self, screen, clock)
         pygame.quit()
@@ -155,126 +147,6 @@ def main():
 
     game = Pong(1, 5, 10)
     game.run()
-
-# # Global settings
-#     point_limit = 10            #
-#     paddle_height = 100         #
-#     paddle_speed = 3            #
-#     ball_acceleration = 0.05    #
-#     paddle_radius = 10          #
-
-#     running = True
-#     lplayer_pos = 450 #
-#     point = [0, 0] #
-#     ball_max_speed = [0, 0] #
-#     ball_bonce = 0     #
-#     max_exchange = 0       #
-#     exchange = 0        #
-#     rplayer_pos = 450 #
-#     IA_target = 450
-#     IA_counter = 0
-#     ball_speed = pygame.Vector2( [3, random.uniform(-1, 1)] ) #
-#     ball_pos = pygame.Vector2( 50, 450) #
-
-#     pygame.init()
-#     screen = pygame.display.set_mode((1200, 900))
-#     clock = pygame.time.Clock()
-#     pygame.display.set_caption("PONG")
-
-#     for_json = {}
-#     for_json["running"] = True
-#     for_json["point"] = [0, 0]
-#     for_json["lplayer_pos"] = lplayer_pos
-#     for_json["lplayer_size"] = paddle_height
-#     for_json["rplayer_pos"] = rplayer_pos
-#     for_json["rplayer_size"] = paddle_height
-#     for_json["player_speed"] = paddle_speed
-#     for_json["ball_pos"] = ball_pos
-#     for_json["ball_speed"] = ball_speed
-
-#     while running:
-
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-#                 print_stat()
-
-#         keys = pygame.key.get_pressed()
-
-#         if point[0] == point_limit or point[1] == point_limit or keys[pygame.K_ESCAPE]:
-#             running = False
-#             print_stat()
-        
-#         if keys[pygame.K_UP] and rplayer_pos > 0:
-#             rplayer_pos -= paddle_speed
-
-#         elif keys[pygame.K_DOWN] and rplayer_pos < 900:
-#             rplayer_pos += paddle_speed
-
-#         # ball move
-#         ball_pos += ball_speed
-
-#         if 55 > ball_pos.x > (55 + ( 2 * ball_speed.x)) and ball_speed.x < 0 and lplayer_pos - (paddle_height / 2 + 5) < ball_pos.y < lplayer_pos + (paddle_height / 2 + 5):
-#             ball_speed.x *= -(1 + ball_acceleration) 
-#             ball_speed.y = (ball_pos.y - lplayer_pos) / paddle_radius
-
-#         elif 1145 < ball_pos.x < (1145 + ( 2 * ball_speed.x)) and ball_speed.x > 0 and rplayer_pos - (paddle_height / 2 + 5) < ball_pos.y < rplayer_pos + (paddle_height / 2 + 5):
-#             ball_speed.x *= -(1 + ball_acceleration)
-#             ball_speed.y = (ball_pos.y - rplayer_pos) / paddle_radius
-
-#         if ball_pos.y < 5 and ball_speed.y < 0 or ball_pos.y > 895 and ball_speed.y > 0:
-#             ball_speed.y *= -1
-#             ball_bonce += 1
-        
-#         if ball_pos.x > 1200:
-#             ball_pos = pygame.Vector2( 1200 - 50, 450 )
-#             ball_speed = pygame.Vector2(-3, random.uniform(-1, 1))
-#             lplayer_pos = 450
-#             rplayer_pos = 450
-#             point[0] += 1
-#             print(f"{point[0]} IA | Player {point[1]}")
-#             screen.fill("black")
-#             if exchange > max_exchange:
-#                 max_exchange = exchange
-#             exchange = 0
-#             pygame.time.delay(500)
-
-#         if ball_pos.x < 0:
-#             ball_pos = pygame.Vector2( 50, 450 )
-#             ball_speed = pygame.Vector2(3, random.uniform(-1, 1))
-#             lplayer_pos = 450
-#             rplayer_pos = 450
-#             point[1] += 1
-#             print(f"{point[0]} IA | Player {point[1]}")
-#             screen.fill("black")
-#             if exchange > max_exchange:
-#                 max_exchange = exchange
-#             exchange = 0
-#             pygame.time.delay(500)
-
-#         # IA move
-
-
-#         if IA_counter == 0:
-#             IA_target = path_estimation( 1, ball_pos.copy(), ball_speed.copy(), 20 )
-
-#         IA_counter += 1
-
-#         if IA_counter > 240:
-#             IA_counter = 0
-        
-#         lplayer_pos = paddle_move( lplayer_pos, IA_target, paddle_speed )
-
-#         # pygame.display.flip()
-#         for_json["lplayer_pos"] = lplayer_pos
-#         for_json["rplayer_pos"] = rplayer_pos
-#         for_json["ball_pos"] = ball_pos
-
-#         widow(for_json, screen, clock)
-
-#         clock.tick(240) / 1000
-
-#     pygame.quit()
 
 
 if __name__ == "__main__":
