82e924e22c3cd2305cb9d8bc88ba66ed57541770
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2024-03-20 09:43:10 +0100

    cleaning code

--- Django_data/apps/Game_tbl/models.py
+++ Django_data/apps/Game_tbl/models.py
@@ -3,16 +3,17 @@ from django.conf import settings
 
 class PlayerInfo(models.Model):
 
-    me = models.OneToOneField(
+    user = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name='toPlayerInfo',
         on_delete=models.CASCADE, primary_key=True,
     )
 
-    ball_touched = models.IntegerField(default=0)
     victory = models.BooleanField()
     score = models.IntegerField(default=0)
     rebounds = models.IntegerField(default=0)
     max_ball_speed = models.IntegerField(default=0)
+
+
 
 class GameHistory(models.Model):
 
