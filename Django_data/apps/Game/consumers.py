b595d686b1546fece5609415510a5fa308598be1
DoubleV37 <valentin.v3719@gmail.com>
2024-03-20 14:31:22 +0100

    Add game backend in the game app django

--- Django_data/apps/Game/consumers.py
+++ Django_data/apps/Game/consumers.py
@@ -1,5 +1,6 @@
-import json
+import json, asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .pong import Pong, ai_brain
 
 class PongConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
@@ -9,6 +10,8 @@ class PongConsumer(AsyncWebsocketConsumer):
 			self.channel_name
 		)
 		await self.accept()
+		self.pong = Pong(1 , 2 , 10)
+		await self.runGame()
 		self.paddleL = 0.5
 		self.paddleR = 0.5
 		self.ballX = 0.5
@@ -30,5 +33,32 @@ class PongConsumer(AsyncWebsocketConsumer):
 			self.paddleL = self.paddleL + 0.1
 		await self.sendMessage(self.paddleR , self.paddleL , self.ballX , self.ballY)
 
-	async def sendMessage(self , paddleR , paddleL , ballX , ballY):
-		await self.send(text_data = json.dumps({"paddleR" : paddleR , "paddleL" : paddleL , "ballX" : ballX , "ballY" : ballY , "type" : "sendMessage"}))
+	async def sendMessage(self , paddleL , paddleR , ballX , ballY):
+		await self.send(text_data = json.dumps({"paddleL" : paddleL , "paddleR" : paddleR , "ballX" : ballX , "ballY" : ballY , "type" : "sendMessage"}))
+
+	async def runGame(self):
+		while self.pong.running:
+			# ia move
+			self.pong.player_pos[0] = ai_brain(self.pong, 1, 20)
+			self.pong.player_pos[1] = ai_brain(self.pong, 2, 20)
+			# ball move
+			self.pong.ball_walk()
+			# paddle bounce
+			if self.pong.ball_pos[0] < 60 and self.pong.ball_speed[0] < 0:
+				self.pong.paddle_bounce(0)
+			elif self.pong.ball_pos[0] > 1140 and self.pong.ball_speed[0] > 0:
+				self.pong.paddle_bounce(1)
+			# wall bounce
+			if self.pong.ball_pos[1] < 5 and self.pong.ball_speed[1] < 0 or self.pong.ball_pos[1] > 895 and self.pong.ball_speed[1] > 0:
+				self.pong.ball_speed[1] *= -1
+				self.pong.ball_bonce += 1
+			# point
+			if self.pong.ball_pos[0] > 1200:
+				self.pong.update_score(0)
+			if self.pong.ball_pos[0] < 0:
+				self.pong.update_score(1)
+			await self.sendMessage(self.pong.player_pos[0]/900 , self.pong.player_pos[1]/900 , self.pong.ball_pos[0]/1200 , self.pong.ball_pos[1]/900)
+			await asyncio.sleep(1/240)
+			# pygame display #
+			# display_game(self, screen)
+			# clock.tick(240)
