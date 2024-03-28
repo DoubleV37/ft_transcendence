007b94c6423f2a167b0cdf0e095387cb30c0cdd3
cmassera <cmassera@student.42angouleme.fr>
2024-03-28 11:22:08 +0100

    powerup first version

--- Django_data/apps/Game/pong.py
+++ Django_data/apps/Game/pong.py
@@ -1,14 +1,5 @@
-# import pygame
 import random
 import math
-
-# def display_game(Pong, screen):
-#     screen.fill("black")
-#     pygame.draw.rect(screen, "white", (598, 0, 4, 900))
-#     pygame.draw.rect(screen, "white", (40, Pong.player_pos[0] - (Pong.player_size[0] / 2), 10, Pong.player_size[0]))
-#     pygame.draw.rect(screen, "white", (1150, Pong.player_pos[1] - (Pong.player_size[1] / 2), 10, Pong.player_size[1]))
-#     pygame.draw.circle(screen, "white", (Pong.ball_pos[0], Pong.ball_pos[1]), 12)
-#     pygame.display.flip()
 
 
 def ai_brain( Pong, player, lvl ):
@@ -97,33 +88,6 @@ class Pong():
 				self.player_size[0] = 80
 				self.player_size[1] = 150
 
-	# def powerup_run(self):
-	# 	if self.powerup == False:
-	# 		return
-	# 	self.powerup_pos[1] += self.powerup_speed
-	# 	if self.powerup_time > 1:
-	# 		self.powerup_time -= 1
-	# 		if self.powerup_time == 10 * 240:
-	# 			self.player_size[0] = 100
-	# 			self.player_size[1] = 100
-	# 	else:
-	# 		seld.powerup_speed = 2
-	# 		if self.powerup_pos[1] > 850:
-	# 			self.powerup_pos[1] = 50
-	# 			self.powerup_time = 5 * 240
-	# 			self.powerup_speed = 0
-	# 		elif self.powerup_pos[0] < self.ball_pos[0] and self.ball_pos[0] < self.powerup_pos[0] + self.powerup_size and self.powerup_pos[1] < self.ball_pos[1] and self.ball_pos[1] < self.powerup_pos[1] + self.powerup_size:
-	# 			self.powerup_pos[1] = 50
-	# 			self.powerup_time = 15 * 240
-	# 			self.powerup_speed = 0
-	# 			if self.ball_speed[0] > 0:
-	# 				self.powerup_count[0] += 1
-	# 				self.player_size[0] = 150
-	# 				self.player_size[1] = 80
-	# 			else:
-	# 				self.powerup_count[1] += 1
-	# 				self.player_size[0] = 80
-	# 				self.player_size[1] = 150
 
 	def stats(self, player):
 		self.exchange += 1
@@ -175,52 +139,3 @@ class Pong():
 	def ball_walk(self):
 		self.ball_pos[0] += self.ball_speed[0]
 		self.ball_pos[1] += self.ball_speed[1]
-
-#     def run(self):
-#         # pygame.init()
-#         # screen = pygame.display.set_mode((1200, 900))
-#         # clock = pygame.time.Clock()
-#         # pygame.display.set_caption("PONG")
-#         while self.running:
-#             # pygame move #
-#             keys = pygame.key.get_pressed()
-#             if self.point[0] == self.point_limit or self.point[1] == self.point_limit or keys[pygame.K_ESCAPE]:
-#                 self.running = False
-#                 self.print_stats()
-#             if keys[pygame.K_UP] and self.player_pos[1] > 0:
-#                 self.player_pos[1] -= self.player_speed
-#             elif keys[pygame.K_DOWN] and self.player_pos[1] < 900:
-#                 self.player_pos[1] += self.player_speed
-
-#             # ia move
-#             self.player_pos[0] = ai_brain(self, 1, 20)
-#             self.player_pos[1] = ai_brain(self, 2, 20)
-#             # ball move
-#             self.ball_walk()
-#             # paddle bounce
-#             if self.ball_pos[0] < 60 and self.ball_speed[0] < 0:
-#                 self.paddle_bounce(0)
-#             elif self.ball_pos[0] > 1140 and self.ball_speed[0] > 0:
-#                 self.paddle_bounce(1)
-#             # wall bounce
-#             if self.ball_pos[1] < 5 and self.ball_speed[1] < 0 or self.ball_pos[1] > 895 and self.ball_speed[1] > 0:
-#                 self.ball_speed[1] *= -1
-#                 self.ball_bonce += 1
-#             # point
-#             if self.ball_pos[0] > 1200:
-#                 self.update_score(0)
-#             if self.ball_pos[0] < 0:
-#                 self.update_score(1)
-#             # pygame display #
-#             display_game(self, screen)
-#             clock.tick(240)
-#         pygame.quit()
-
-
-# def main():
-#     game = Pong(1, 2, 10)
-#     game.run()
-
-
-# if __name__ == "__main__":
-#     main()
