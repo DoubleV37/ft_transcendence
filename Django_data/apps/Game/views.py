5784eff8ecbc2d7ebfcdefc73d7b8b71a87f4c35
Guillaume Azzopardi <gazzopar@1E5.42angouleme.fr>
2024-04-02 15:55:26 +0200

    First iteration of Game Front screen + score and players

--- Django_data/apps/Game/views.py
+++ Django_data/apps/Game/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
+from apps.Auth.models import User
+from django.http import JsonResponse
 
-def gamePage(request, *args, **kwargs):
-    if not request.user.is_authenticated:
-        return redirect("/")
-    context = {}
-    return render(request, "Game/gamePage.html", context)
+def gamePage(request, _id=None, *args, **kwargs ):
+    try:
+        if _id is None:
+            _user = request.user
+            _type = 'himself'
+        else:
+            _user = User.objects.get(idUser=_id)
+            _type = 'foreign'
+        _username = _user.username
+        _avatar = _user.avatar.url
+        return render(request, "Game/gamePage.html", 
+                    {
+                    'profil_picture': _avatar,
+                    'username': _username,
+                    })
+    except User.objects.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Wrong ID'})
 
 def gameParameters(request):
     return render(request, "Game/Parameters.html")
