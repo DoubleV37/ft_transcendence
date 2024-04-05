6df53cc0f5ef0d4c8ca8bba0822b26d2babbd5fc
jean <jduval@3E6.42angouleme.fr>
2024-04-05 14:54:35 +0200

    Front OK
    DONE:
     -> Set events of Parameters.
    TODO:
     -> Nothing

--- Django_data/static/js/Game/Parameters_Events.js
+++ Django_data/static/js/Game/Parameters_Events.js
@@ -1,60 +1,41 @@
-function parameters_SetEvents() {
+function parameters_SetEvents () {
   const minusBtns = document.querySelectorAll(".minus");
   const plusBtns = document.querySelectorAll(".plus");
-  const inputBoxes = document.querySelectorAll(".input-box");
 
-  updateButtonStates();
+  parameters_SetPage();
 
   minusBtns.forEach((btn) => btn.addEventListener("click", decreaseValue));
   plusBtns.forEach((btn) => btn.addEventListener("click", increaseValue));
-  inputBoxes.forEach((box) =>
-    box.addEventListener("input", handleQuantityChange),
-  );
 
-  //let element = document.getElementById(elementId)
+  let element = document.getElementById("SELECT_Player");
+  element.addEventListener("click", parameters_SelectOpponent);
+
+  element = document.getElementById("SELECT_ai");
+  element.addEventListener("click", parameters_SelectOpponent);
+
+  element = document.getElementById("StartButton");
+  element.addEventListener("click", parameters_StartGame);
+
+  element = document.getElementById("switchCheckLabelTop");
+  element.addEventListener("click", parameters_SetPowerUp);
 }
 
-function parameters_DelEvents() {
+function parameters_DelEvents () {
   const minusBtns = document.querySelectorAll(".minus");
   const plusBtns = document.querySelectorAll(".plus");
-  const inputBoxes = document.querySelectorAll(".input-box");
 
   minusBtns.forEach((btn) => btn.removeEventListener("click", decreaseValue));
   plusBtns.forEach((btn) => btn.removeEventListener("click", increaseValue));
-  inputBoxes.forEach((box) =>
-    box.removeEventListener("input", handleQuantityChange),
-  );
-}
 
-function updateButtonStates() {
-  document.querySelectorAll(".input-box").forEach((inputBox) => {
-    const value = parseInt(inputBox.value);
-    const minusBtn = inputBox.parentNode.querySelector(".minus");
-    const plusBtn = inputBox.parentNode.querySelector(".plus");
-    minusBtn.disabled = value <= 1;
-    plusBtn.disabled = value >= parseInt(inputBox.max);
-  });
-}
+  let element = document.getElementById("SELECT_Player");
+  element.removeEventListener("click", parameters_SelectOpponent);
 
-function decreaseValue() {
-  const inputBox = this.parentNode.querySelector(".input-box");
-  let value = parseInt(inputBox.value);
-  value = isNaN(value) ? 1 : Math.max(value - 1, 1);
-  inputBox.value = value;
-  updateButtonStates();
-  handleQuantityChange.call(inputBox);
-}
+  element = document.getElementById("SELECT_ai");
+  element.removeEventListener("click", parameters_SelectOpponent);
 
-function increaseValue() {
-  const inputBox = this.parentNode.querySelector(".input-box");
-  let value = parseInt(inputBox.value);
-  value = isNaN(value) ? 1 : Math.min(value + 1, parseInt(inputBox.max));
-  inputBox.value = value;
-  updateButtonStates();
-  handleQuantityChange.call(inputBox);
-}
+  element = document.getElementById("StartButton");
+  element.removeEventListener("click", parameters_StartGame);
 
-function handleQuantityChange() {
-  const value = parseInt(this.value);
-  console.log("Quantity changed:", value);
+  element = document.getElementById("switchCheckLabelTop");
+  element.removeEventListener("click", parameters_SetPowerUp);
 }
