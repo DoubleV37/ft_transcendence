8f36f7627ed85f7cad0b0b46872d47098a3ba65e
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2024-04-15 16:41:11 +0200

    See prev commit

--- Django_data/apps/Auth/validators.py
+++ Django_data/apps/Auth/validators.py
@@ -7,3 +7,12 @@ def validate_file_extension(value: str):
     valid_extensions = ['.jpg', '.png', '.xlsx', '.xls']
     if not value.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
+
+# def image_size(value: str):
+#         image = cleaned_data.get('image', False)
+#         if image:
+#             if image._size > 4*1024*1024:
+#                 raise ValidationError("Image file too large ( > 4mb )")
+#             return image
+#         else:
+#             raise ValidationError("Couldn't read uploaded image")
