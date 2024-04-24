d1362cd8f0e142fd6bae49c18bf8f9544b343681
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2024-04-24 10:36:40 +0200

    New quick db files to branch game

--- Django_data/apps/Game/models.py
+++ Django_data/apps/Game/models.py
@@ -1,30 +1,35 @@
 from django.db import models
 from django.conf import settings
 
+
 class MatchmakingUser(models.Model):
     channel_name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.channel_name
 
-class Games(models.Model):
-	idGame = models.CharField(max_length=255, unique=True)
-	nb_users = models.IntegerField(default=0)
-	running = models.BooleanField(default=False)
-	date = models.DateTimeField(auto_now_add=True)
-	duration = models.IntegerField(default=0)
-	pwr_up = models.BooleanField(default=False)
-	nb_rounds = models.IntegerField(default=0)
-	in_tournament = models.BooleanField(default=False)
 
-	def __str__(self):
-		return self.idGame
+class Games(models.Model):
+    # field for game handle
+    idGame = models.CharField(max_length=255, unique=True)
+    nb_users = models.IntegerField(default=0)
+    running = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+    duration = models.IntegerField(default=0)
+    pwr_up = models.BooleanField(default=False)
+    nb_rounds = models.IntegerField(default=0)
+    in_tournament = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.idGame
+
 
 class UserGame(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	game = models.ForeignKey(Games, on_delete=models.CASCADE)
-	score = models.IntegerField(default=0)
-	winner = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    game = models.ForeignKey(Games, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    winner = models.BooleanField(default=False)
 
-	def __str__(self):
-		return self.user.username + " " + self.game.idGame
+    def __str__(self):
+        return self.user.username + " " + self.game.idGame
