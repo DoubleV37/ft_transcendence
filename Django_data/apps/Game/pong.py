9f81306d1f9a7bfb99346496570c05f2f83ac061
Kierkenstein <78741529+Kierkenstein@users.noreply.github.com>
2024-05-05 14:13:27 +0200

    Game/fix/ballspeed again (#139)
    * BUF GIX
    
    * Fixed for the third time the game.py modifications of the ball speed FFSCharles's Job here is done
    
    ---------
    
    Co-authored-by: Guillaume Azzopardi <gazzopar@2D1.42angouleme.fr>

--- Django_data/apps/Game/pong.py
+++ Django_data/apps/Game/pong.py
@@ -93,7 +93,6 @@ class Pong:
         self.point[player] += 1
         if self.point[player] == self.point_limit:
             self.running = False
-            # self.print_stats()
         self.player_pos[0] = 450
         self.player_pos[1] = 450
         self.engage_time = self.time + 1000
@@ -121,14 +120,6 @@ class Pong:
             "max_exchange": self.max_exchange,
         }
         return dic_stats
-        # if self.point[0] > self.point[1]:
-        # 	print("\nPlayer 1 WINS\n")
-        # else:
-        # 	print("\nPlayer 2 WINS\n")
-        # print("score: ", self.point[0], " - ", self.point[1])
-        # print("max speed: ", round(self.ball_max_speed[0] * 10, 1), " - ", round(self.ball_max_speed[1] * 10, 1))
-        # print("bounce: ", self.ball_bonce)
-        # print("max exchange: ", self.max_exchange)
 
     def paddle_bounce(self, player):
         if player == 0:
