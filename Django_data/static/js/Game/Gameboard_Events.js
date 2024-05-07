bbc4f239621b35a619ac101424a012b6441a12d4
Guillaume Azzopardi <guillaume.dambraazzopardi@gmail.com>
2024-05-07 19:37:49 +0200

    Added a (kind of) dynamic notification red dot on the Request header to inform the user that he has pending friend requests

--- Django_data/static/js/Game/Gameboard_Events.js
+++ Django_data/static/js/Game/Gameboard_Events.js
@@ -10,33 +10,27 @@ function gameboard_Callbacks () {
     let scoreL = document.getElementById('score-left').textContent;
     let scoreR = document.getElementById('score-right').textContent;
 
-    // Conversion des scores en nombres si nécessaire
     scoreL = parseInt(scoreL, 10);
     scoreR = parseInt(scoreR, 10);
 
     let totalRounds = document.getElementById('NbRounds').textContent;
     totalRounds = parseInt(totalRounds, 10);
         
-      // Calculer le pourcentage de progression pour chaque joueur
     function calculateProgress(score, totalRounds) {
-      // Si le score est 0, retourner 1% pour indiquer une petite progression
       if (score === 0 && totalRounds === 0) {
         return 50;
       }
       else if (score === 0) {
           return 6;
       }
-      // Sinon, calculer le pourcentage normal
       return (score / totalRounds) * 100;
     }
 
-    // Mettre à jour la barre de progression pour chaque joueur
     function updateProgressBar(score, progressBarId) {
       let progressPercentage = calculateProgress(score, totalRounds);
       progressBarId.style.setProperty('--final-width', `${progressPercentage}%`);
     }
 
-    // Mettre à jour les barres de progression
     updateProgressBar(scoreL, elL);
     updateProgressBar(scoreR, elR);
 }
--- Django_data/static/js/Game/Stats_Events.js
+++ Django_data/static/js/Game/Stats_Events.js
@@ -1,5 +1,4 @@
 function stats_SetEvents () {
-
     manageGames();
     manageWins();
 }
@@ -14,11 +13,9 @@ function manageGames() {
   let rGames = document.getElementById('regular-games').textContent;
   let tGames = document.getElementById('tournament-games').textContent;
 
-  // Conversion des scores en nombres si nécessaire
   rGames = parseInt(rGames, 10);
   tGames = parseInt(tGames, 10);
 
-  // Calculer le pourcentage de progression pour chaque joueur
   function calculateProgressGames(Games, totalGames) {
       if (Games === 0 && totalGames === 0) {
         return 50;
@@ -26,17 +23,14 @@ function manageGames() {
       else if (Games === 0) {
           return 6;
       }
-      // Sinon, calculer le pourcentage normal
       return (Games / totalGames) * 100;
   }
 
-  // Mettre à jour la barre de progression pour chaque joueur
   async function updateProgressBarGames(Games, progressBarId) {
     let progressPercentage = calculateProgressGames(Games, totalGames);
     progressBarId.style.setProperty('--final-width', `${progressPercentage}%`);
   }
 
-  // Mettre à jour les barres de progression
   updateProgressBarGames(rGames, elL);
   updateProgressBarGames(tGames, elR);
 }
@@ -51,11 +45,9 @@ function manageWins() {
   let rWins = document.getElementById('regular-wins').textContent;
   let tWins = document.getElementById('tournament-wins').textContent;
 
-  // Conversion des scores en nombres si nécessaire
   rWins = parseInt(rWins, 10);
   tWins = parseInt(tWins, 10);
 
-  // Calculer le pourcentage de progression pour chaque joueur
   function calculateProgressWins(Wins, totalWins) {
       if (Wins === 0 && totalWins === 0) {
         return 50;
@@ -63,17 +55,14 @@ function manageWins() {
       else if (Wins === 0) {
           return 6;
       }
-      // Sinon, calculer le pourcentage normal
       return (Wins / totalWins) * 100;
   }
 
-  // Mettre à jour la barre de progression pour chaque joueur
   async function updateProgressBarWins(Wins, progressBarId) {
     let progressPercentage = calculateProgressWins(Wins, totalWins);
     progressBarId.style.setProperty('--final-width', `${progressPercentage}%`);
   }
 
-  // Mettre à jour les barres de progression
   updateProgressBarWins(rWins, wL);
   updateProgressBarWins(tWins, wR);
 }
