715f51d59be6dbbf79af64e97a03f9271600f13f
Guillaume Azzopardi <guillaume.dambraazzopardi@gmail.com>
2024-05-07 16:55:41 +0200

    Full rework of CSS progress bar animation using better code, keyframes, and triggering properly on FIRST load

--- Django_data/static/js/Game/Gameboard_Events.js
+++ Django_data/static/js/Game/Gameboard_Events.js
@@ -1,6 +1,10 @@
 function gameboard_SetEvents () {
+    gameboard_Callbacks();
+}
 
-	const elL = document.getElementById("left");
+function gameboard_Callbacks () {
+
+    const elL = document.getElementById("left");
     const elR = document.getElementById("right");
     
     let scoreL = document.getElementById('score-left').textContent;
@@ -10,14 +14,17 @@ function gameboard_SetEvents () {
     scoreL = parseInt(scoreL, 10);
     scoreR = parseInt(scoreR, 10);
 
-	let totalRounds = document.getElementById('NbRounds').textContent;
-	totalRounds = parseInt(totalRounds, 10);
+    let totalRounds = document.getElementById('NbRounds').textContent;
+    totalRounds = parseInt(totalRounds, 10);
         
       // Calculer le pourcentage de progression pour chaque joueur
     function calculateProgress(score, totalRounds) {
       // Si le score est 0, retourner 1% pour indiquer une petite progression
-      if (score === 0) {
-          return 5;
+      if (score === 0 && totalRounds === 0) {
+        return 50;
+      }
+      else if (score === 0) {
+          return 6;
       }
       // Sinon, calculer le pourcentage normal
       return (score / totalRounds) * 100;
@@ -26,7 +33,7 @@ function gameboard_SetEvents () {
     // Mettre à jour la barre de progression pour chaque joueur
     function updateProgressBar(score, progressBarId) {
       let progressPercentage = calculateProgress(score, totalRounds);
-      progressBarId.style.width = `${progressPercentage}%`;
+      progressBarId.style.setProperty('--final-width', `${progressPercentage}%`);
     }
 
     // Mettre à jour les barres de progression
--- Django_data/static/js/Game/Stats_Events.js
+++ Django_data/static/js/Game/Stats_Events.js
@@ -1,79 +1,79 @@
 function stats_SetEvents () {
-    let totalGames = document.getElementById('NbGames').textContent;
-    totalGames = parseInt(totalGames, 10);
 
-	  const elL = document.getElementById("left-games");
-    const elR = document.getElementById("right-games");
-    
-    let rGames = document.getElementById('regular-games').textContent;
-    let tGames = document.getElementById('tournament-games').textContent;
+    manageGames();
+    manageWins();
+}
 
-    // Conversion des scores en nombres si nécessaire
-    rGames = parseInt(rGames, 10);
-    tGames = parseInt(tGames, 10);
+function manageGames() {
+  let totalGames = document.getElementById('NbGames').textContent;
+  totalGames = parseInt(totalGames, 10);
 
-    let totalWins = document.getElementById('Victories').textContent;
-    totalWins = parseInt(totalWins, 10);
+  const elL = document.getElementById("left-games");
+  const elR = document.getElementById("right-games");
 
-    const wL = document.getElementById("left-wins");
-    const wR = document.getElementById("right-wins");
-    
-    let rWins = document.getElementById('regular-wins').textContent;
-    let tWins = document.getElementById('tournament-wins').textContent;
+  let rGames = document.getElementById('regular-games').textContent;
+  let tGames = document.getElementById('tournament-games').textContent;
 
-    // Conversion des scores en nombres si nécessaire
-    rWins = parseInt(rWins, 10);
-    tWins = parseInt(tWins, 10);
-        
-      // Calculer le pourcentage de progression pour chaque joueur
-    function calculateProgressGames(tGames, totalGames) {
-      // Si le score est 0, retourner 1% pour indiquer une petite progression
-      if (tGames === 0 && rGames != 0) {
-          return 5;
+  // Conversion des scores en nombres si nécessaire
+  rGames = parseInt(rGames, 10);
+  tGames = parseInt(tGames, 10);
+
+  // Calculer le pourcentage de progression pour chaque joueur
+  function calculateProgressGames(Games, totalGames) {
+      if (Games === 0 && totalGames === 0) {
+        return 50;
       }
-      else if (tGames != 0 && rGames === 0) {
-          return 5;
-      }
-      else if (tGames === 0 && rGames === 0) {
-          return 50;
+      else if (Games === 0) {
+          return 6;
       }
       // Sinon, calculer le pourcentage normal
-      return (tGames / totalGames) * 100;
-    }
+      return (Games / totalGames) * 100;
+  }
 
-    // Mettre à jour la barre de progression pour chaque joueur
-    async function updateProgressBarGames(tGames, progressBarId) {
-      let progressPercentage = calculateProgressGames(tGames, totalGames);
-      progressBarId.style.width = `${progressPercentage}%`;
-    }
+  // Mettre à jour la barre de progression pour chaque joueur
+  async function updateProgressBarGames(Games, progressBarId) {
+    let progressPercentage = calculateProgressGames(Games, totalGames);
+    progressBarId.style.setProperty('--final-width', `${progressPercentage}%`);
+  }
 
-    // Mettre à jour les barres de progression
-    updateProgressBarGames(rGames, elL);
-    updateProgressBarGames(tGames, elR);
-        
-      // Calculer le pourcentage de progression pour chaque joueur
-    function calculateProgressWins(tWins, totalWins) {
-      // Si le score est 0, retourner 1% pour indiquer une petite progression
-      if (tWins === 0 && rWins != 0) {
-          return 5;
+  // Mettre à jour les barres de progression
+  updateProgressBarGames(rGames, elL);
+  updateProgressBarGames(tGames, elR);
+}
+
+function manageWins() {
+  let totalWins = document.getElementById('Victories').textContent;
+  totalWins = parseInt(totalWins, 10);
+
+  const wL = document.getElementById("left-wins");
+  const wR = document.getElementById("right-wins");
+
+  let rWins = document.getElementById('regular-wins').textContent;
+  let tWins = document.getElementById('tournament-wins').textContent;
+
+  // Conversion des scores en nombres si nécessaire
+  rWins = parseInt(rWins, 10);
+  tWins = parseInt(tWins, 10);
+
+  // Calculer le pourcentage de progression pour chaque joueur
+  function calculateProgressWins(Wins, totalWins) {
+      if (Wins === 0 && totalWins === 0) {
+        return 50;
       }
-      else if (tWins != 0 && rWins === 0) {
-          return 5;
-      }
-      else if (tWins === 0 && rWins === 0) {
-          return 50;
+      else if (Wins === 0) {
+          return 6;
       }
       // Sinon, calculer le pourcentage normal
-      return (tWins / totalWins) * 100;
-    }
+      return (Wins / totalWins) * 100;
+  }
 
-    // Mettre à jour la barre de progression pour chaque joueur
-    async function updateProgressBarWins(tWins, progressBarId) {
-      let progressPercentage = calculateProgressWins(tWins, totalWins);
-      progressBarId.style.width = `${progressPercentage}%`;
-    }
+  // Mettre à jour la barre de progression pour chaque joueur
+  async function updateProgressBarWins(Wins, progressBarId) {
+    let progressPercentage = calculateProgressWins(Wins, totalWins);
+    progressBarId.style.setProperty('--final-width', `${progressPercentage}%`);
+  }
 
-    // Mettre à jour les barres de progression
-    updateProgressBarWins(rWins, wL);
-    updateProgressBarWins(tWins, wR);
+  // Mettre à jour les barres de progression
+  updateProgressBarWins(rWins, wL);
+  updateProgressBarWins(tWins, wR);
 }
