3c8df2dd2963297abb1b1439ccbacc5fe6428313
DoubleV37 <valentin.v3719@gmail.com>
2024-05-09 00:22:13 +0200

    remove old version of matchmaking and room for multigame

--- Django_data/apps/Game/routing.py
+++ Django_data/apps/Game/routing.py
@@ -1,10 +1,8 @@
 from django.urls import path , re_path
-from . import consumers , consumers_matchmaking , consumers_multiplayer
+from . import consumers
 
 websocket_urlpatterns = [
 	re_path("wss/game/solo" , consumers.SoloPongConsumer.as_asgi()) ,
-	re_path("wss/game/matchmaking" , consumers_matchmaking.MatchmakingPongConsumer.as_asgi()) ,
-	re_path(r'wss/game/(?P<room_name>\w+/)$' , consumers_multiplayer.MultiPongConsumer.as_asgi()) ,
 ]
 
 
